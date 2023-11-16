e81a373ff10183214d14c30e22c7e0a772f23cd0
sravyavulisetty <sravyavulisetty269@gmail.com>
2023-11-16 10:27:13 -0500

    mobile res

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       <div style={{backgroundImage: `url(${vector})`}} className='absolute top-1/2 right-1/2 translate-x-1/2 bg-zinc-900 bg-no-repeat bg-top bg-auto rounded-lg min-h-screen w-3/4 z-10'>
         <div className='flex flex-col items-center justify-center m-2 mt-12'>
           <p className='text-white text-3xl font-bold'>Our Collection</p>
-          <p className='text-[#6F757C] mx-72 text-center mt-4 font-semibold'>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+          <p className='text-[#6F757C] lg:mx-60 md:mx-24 sm:mx-20 text-center mt-4 font-semibold'>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
           <div className='flex gap-2 items-center justify-center mt-8'>
             <button className='text-white text-sm p-2 rounded-lg font-semibold' style={styles2} onClick={()=>setAvailproducts(false)}>All Products</button>
             <button className='text-white text-sm pl-4 rounded-lg p-2 font-semibold' style={styles} onClick={()=>setAvailproducts(true)}>Available Now</button>
