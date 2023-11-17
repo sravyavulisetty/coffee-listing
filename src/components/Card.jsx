1eaaff6bed8e538e19b563dbdb4f68d9fbec7e55
sravyavulisetty <sravyavulisetty269@gmail.com>
2023-11-17 12:37:01 -0500

    changes made

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 import star from '../Star_fill.svg';
 import nostar from '../Star.svg';
 const Card = ({image, name, price, rating, votes, available, popular}) => {
@@ -8,8 +9,8 @@ const Card = ({image, name, price, rating, votes, available, popular}) => {
       <img src={image} className='relative rounded-xl border border-black w-68 h-40' alt='img'></img>
       {popular ? <p className='absolute m-3 p-1 px-3 bg-[#F6C768] rounded-xl text-xs font-semibold'>Popular</p> :""}
       <div className='flex flex-row justify-between pt-4'>
-        <p className='text-white font-semibold'>{name}</p>
-        <p className='bg-[#BEE3CC] text-black rounded-md p-1 text-xs font-semibold'>{price}</p>
+        <p className='text-white font-semibold font-font1'>{name}</p>
+        <p className='bg-[#BEE3CC] text-black rounded-md p-1 text-xs font-font4 font-semibold'>{price}</p>
       </div>
       <div className='flex flex-row items-center pt-4'>
       {rating!==null ? 
@@ -24,11 +25,11 @@ const Card = ({image, name, price, rating, votes, available, popular}) => {
         </>
         }
         {votes>0 ?
-        <p className='text-[#6F757C] pl-1 text-sm font-semibold'>({votes}) votes</p>
+        <p className='text-[#6F757C] pl-1 text-sm font-semibold font-font3'>({votes} votes)</p>
         :
         <p className='text-[#6F757C] pl-1 text-sm font-semibold'></p>
         }
-        {!available ? <p className='text-[#ED735D] font-semibold text-sm pl-14'>Sold Out</p>:""}
+        {!available ? <p className='text-[#ED735D] font-semibold text-sm pl-16'>Sold Out</p>:""}
       </div>
     </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ import {bgcafe} from './src/bg-cafe.jpg';
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend:{},
+    extend:{
+      fontFamily:{
+        fontDM :['DM Sans', 'sans-serif']
+      },
+      fontWeight:{
+        font0:'2rem',
+        font1: '1rem',
+        font2:'0.875rem',
+        font3:'0.625rem',
+        font4:'0.75rem'
+      }
+    },
   plugins: [],
 }
 }
